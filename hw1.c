8d35bc0481dd31927fe4efb14a90564fdc6a92d1
Katherineweng <wengk@rpi.edu>
2020-09-24 00:27:35 -0400

    hw1 with open file

--- hw1.c
+++ hw1.c
@@ -14,6 +14,14 @@
 */
 
 /*---------------------------- GLOBAL VAIRABLES -----------------------------*/
+/*  	
+		OP # 		TYPE
+		01 			RRQ
+		02 			WRQ
+		03 			DATA
+		04 			ACK
+		05 			ERROR
+ */
 
 #define BUFFsize 512
 
@@ -29,17 +37,8 @@ int COUNT = 0;
 /*---------------------------------------------------------------------------*/
 
 /*-------------------------- Struct Declaration -----------------------------*/
-/*  	
-		OP # 		TYPE
-		01 			RRQ
-		02 			WRQ
-		03 			DATA
-		04 			ACK
-		05 			ERROR
- */
 
 typedef struct Packet {
-
 	unsigned short int OpCode; /* ALL, 2 bytes */
 
 	char FileName[BUFFsize]; /* RRQ or WRQ, string*/
@@ -60,6 +59,11 @@ int UDPsetup(int port);
 
 /* Fill struct Packet with given msg */
 void Parse_msg(char* msg, Packet* packet);
+
+FILE* open_file(Packet* packet);
+
+/* Send Error Packet */
+void send_error(int sockfd, SA cliaddr, unsigned short int errCode, char* errMsg);
 
 /*---------------------------------------------------------------------------*/
 
@@ -87,7 +91,8 @@ int main( int argc, char **argv ) {
 	/*----------------------------------- UDP SETUP ------------------------------------*/
 	int sockfd = UDPsetup(start);
 
-	struct sockaddr_in	servaddr, cliaddr;
+	struct sockaddr_in	servaddr;
+	SA cliaddr;
 	socklen_t	len = sizeof(cliaddr);
 	char		msg[MAXLINE];
 	/*----------------------------------------------------------------------------------*/
@@ -123,6 +128,22 @@ int main( int argc, char **argv ) {
 			/* interact with client using a different port */
 			sockfd = UDPsetup(start+COUNT);
 
+			Packet* packet = calloc( 1, sizeof(Packet) );
+
+			Parse_msg(msg, packet);
+
+			/* If the request is not RRQ nor WRQ */
+			if ( packet->OpCode != 1 && packet->OpCode != 2 ) {
+				send_error(sockfd, cliaddr, 0, "INVALID REQUEST");
+			}
+
+			FILE* file = open_file(packet);
+
+			/* if fopen failed */
+			if (file == NULL) {
+				send_error(sockfd, cliaddr, 1, "");
+				exit(0);
+			}
 
 		}
 
@@ -160,7 +181,6 @@ int UDPsetup(int port) {
 
 void Parse_msg(char* msg, Packet* packet) {
 
-	bzero(packet, sizeof(Packet));
 	packet->OpCode = -1;
 	packet->BlockNo = -1;
 	packet->ErrorCode = -1;
@@ -170,24 +190,65 @@ void Parse_msg(char* msg, Packet* packet) {
 	packet->OpCode = ntohs(*opcode_ptr);
 
 	/* RRQ or WRQ */
-	if (packet->OpCode == 1 ||packet->OpCode == 2) {
-		/* Get Filename */
-		char* tmp = msg+2; /* skip first 2 bytes */
-		strcpy(packet->FileName, tmp);
+	if (packet->OpCode == 1 || packet->OpCode == 2) {
+
+		/* Get Filename, skip first 2 bytes */
+		strcpy(packet->FileName, msg+2);
+
 	} 
 	/* DATA or ACK */
 	else if (packet->OpCode == 3 || packet->OpCode == 4) {
+
 		/* bytes 3 and 4 */
 		packet->BlockNo = ntohs(*(opcode_ptr+1));
+
+		if (packet->OpCode == 3) strcpy(packet->Data, msg+4);
 
 	} 
 	/* ERROR */
 	else if (packet->OpCode == 5) {
 
+		/* bytes 3 and 4 */
 		packet->ErrorCode = ntohs(*(opcode_ptr+1));
 
-		/* Get ErrMsg */
-		char* tmp = msg+2; /* skip first 2 bytes */
-		strcpy(packet->ErrMsg, tmp);
+		/* Get ErrMsg, skip first 2 bytes */
+		strcpy(packet->ErrMsg, msg+2);
+
 	}
+}
+
+FILE* open_file(Packet* packet) {
+	FILE* f;
+	if (packet->OpCode == 1) {
+		f = fopen(packet->FileName, "r");
+	} else if (packet->OpCode == 2) {
+		f = fopen(packet->FileName, "w");
+	}
+	return f;
+}
+/*
+Error Codes
+
+   Value     Meaning
+
+   0         Not defined, see error message (if any).
+   1         File not found.
+   2         Access violation.
+   3         Disk full or allocation exceeded.
+   4         Illegal TFTP operation.
+   5         Unknown transfer ID.
+   6         File already exists.
+   7         No such user.
+*/
+void send_error(int sockfd, SA cliaddr, unsigned short int errCode, char* errMsg) {
+	unsigned short int errOp = 5;
+	char buff[BUFFsize+4];
+	memset(buff, 0, BUFFsize+4);
+
+	unsigned short int* ptr = (unsigned short int*) buff;
+	*ptr = htons(errOp); /* Set OP Code*/
+	*(ptr+1) = htons(errCode); /* Set Error Code*/
+	strcpy(buff+4, errMsg); /* Set Error Message */
+
+	Sendto(sockfd, buff, strlen(errMsg)+4, 0, (SA *) &cliaddr, sizeof(cliaddr));
 }
